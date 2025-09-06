26f5550293f097eb69c04d5d269ef85c9e587076
Dawud354 <dawud.a.hussain@gmail.com>
2025-09-06 10:58:59 +0100

    Feat: Implemented SceneManager to manage scenes

--- src/main/java/MainGUI.java
+++ src/main/java/MainGUI.java
@@ -1,16 +1,13 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 
 public class MainGUI extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Hello JavaFX!");
-        Scene scene = new Scene(label, 400, 300);
-        stage.setScene(scene);
-        stage.setTitle("My JavaFX App");
+        SceneManager manager = new SceneManager(stage);
+        manager.showMenu(); // show menu first
+        stage.setTitle("Minesweeper");
         stage.show();
     }
 
---
+++ src/main/java/SceneManager.java
@@ -0,0 +1,54 @@
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+
+
+public class SceneManager {
+    private final Stage stage;
+    private final Scene scene;
+    private final BorderPane root; // permanent root
+
+
+    public SceneManager(Stage stage) {
+        this.stage = stage;
+
+        // permanent root with menu bar on top
+        root = new BorderPane();
+        root.setTop(createMenuBar());
+
+        scene = new Scene(root, 800, 600);
+        stage.setScene(scene);
+    }
+
+    private MenuBar createMenuBar() {
+        MenuBar menuBar = new MenuBar();
+
+        Menu fileMenu = new Menu("File");
+        MenuItem exitItem = new MenuItem("Exit");
+        exitItem.setOnAction(e -> stage.close());
+        fileMenu.getItems().add(exitItem);
+
+        Menu helpMenu = new Menu("Help");
+        MenuItem aboutItem = new MenuItem("About");
+        aboutItem.setOnAction(e -> System.out.println("About clicked"));
+        helpMenu.getItems().add(aboutItem);
+
+        menuBar.getMenus().addAll(fileMenu, helpMenu);
+        return menuBar;
+    }
+
+
+    // Swap only the center
+    public void showMenu() {
+        MenuView menu = new MenuView(this);
+        root.setCenter(menu.getView());
+    }
+
+    public void showGame() {
+        GameView game = new GameView(this);
+        root.setCenter(game.getView());
+    }
+}
